e62c3dd730b11fc2126a67621aca0aa9ac44d2b4
Itsmaheshk <maheshbkamble@coep.sveri.ac.in>
2020-10-31 23:06:24 +0530

    new commit

---
+++ variables.js
@@ -0,0 +1,30 @@
+// we have 3 variables in Js
+
+// 1.var
+// var can be redeclaired & also its value can be changed
+var name= 'Mahesh';  //declaration
+console.log(name);
+
+var name= 'Mahesh';   //redeclaration
+console.log(name);
+
+var name= 'Ramesh'; // revalued
+console.log (name);
+
+
+// 2.let
+// value can be changed but cannot redeclaired
+let rollno= 52;
+console.log(rollno);
+
+rollno=64;
+console.log(rollno);  //revalued
+
+// cannot do "let rollno = 64" i.e. redeclaration of Varible let
+
+
+// 3.const
+//it cannot be redeclaired nor revalued
+
+const id= 2345;
+console.log(id);
