3ae675003f91ad9fdad55705f41bf2be175dedbf
Itsmaheshk <maheshbkamble@coep.sveri.ac.in>
2020-10-31 22:33:44 +0530

    1st commit

---
+++ hello.js
@@ -0,0 +1,10 @@
+console.log('Hey there...! Mahesh is Here')
+
+// we use document.write('Hey there') in HTML to show output. But in pure js we use 
+//console.log()
+// step to execute this code
+// 1. make sure you've installed Node.js on system
+// 2. write code in editor & save
+// 3. Goto command prompt & navigate to file folder location
+// 4. type command- node <filename.js>
+// 5. Thats it. You'll get your output there.
