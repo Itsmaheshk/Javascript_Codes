99c0cbdd2a9622c74094aef46e75bdba84d257af
Itsmaheshk <maheshbkamble@coep.sveri.ac.in>
2020-11-02 22:57:14 +0530

    1st commit

---
+++ nestedif.js
@@ -0,0 +1,40 @@
+let bank = 2 // change bank no here
+if (bank == 1)
+
+{
+ const accountNo= 123213;  // change account no  here
+ if (accountNo== 123213)
+ {
+     console.log('Welcome 123213 in Bank 1');
+ }
+ else if (accountNo == 12345)
+ {
+     console.log( 'Welcome 12345 to bank 1');
+ }
+ else
+ {
+     console.log('Invalid Account in bank 1')
+ }
+}
+
+else if (bank == 2)
+{
+    const accountNo= 123213;  // change account no  here
+    if (accountNo== 123213)
+    {
+        console.log('Welcome 123213 in Bank 2');
+    }
+    else if (accountNo == 12345)
+    {
+        console.log( 'Welcome 12345 to bank 2');
+    }
+    else
+    {
+        console.log('Invalid Account in bank 2')
+    }
+}
+
+else
+{
+    console.log('Invalid Bank')
+}
